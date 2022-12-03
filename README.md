f2b7a2d8d7b53880138039fbf9c3e7dacad110e5
KosaDoge <deadlyinu@gmail.com>
2022-12-04 00:53:57 +0800

    Add windows terminal settings

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 # KosaDoge dotfiles
+Windows 使用 PowerShell ，將 Repository Clone 到 $HOME 內，將相關設定鏈結：
+
+```powershell
+# 設定 PowerShell 的 Prefix
+> New-Item -Path $Profile.CurrentUserAllHosts -Value $Home\.dotfiles\profile.ps1 -ItemType SymbolicLink -Force
+
+# 設定 Windows Terminal
+> New-Item -Path $Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Value $Home\.dotfiles\settings.json -ItemType SymbolicLink -Force
+
+# 設定 lsd 設定
+> New-Item -Path $Env:AppData\lsd -Value $Home\.dotfiles\lsd -ItemType SymbolicLink -Force
+```
+
 
 ## Neovim
 會自動安裝 [Packer](https://github.com/wbthomason/packer.nvim) 來管理 Plugins ，只需要鏈結 `neovim` 目錄的檔案到 `~/.config/nvim` 下：
 
-```shell
-$ ln -s ~/.dotfiles/neovim/init.lua ~/.config/nvim/init.lua
-$ ln -s ~/.dotfiles/neovim/lua ~/.config/nvim/lua
+```powershell
+> New-Item -Path $Env:LocalAppData\nvim\init.lua -Value $Home\.dotfiles\neovim\init.lua -ItemType SymbolicLink -Force
+> New-Item -Path $Env:LocalAppData\nvim\lua -Value $Home\.dotfiles\neovim\lua -ItemType SymbolicLink -Force
 ```
 
 之後開啟 Neovim 後執行 `:PackerSync` 即可。
---
+++ profile.ps1
@@ -0,0 +1,58 @@
+function Write-BranchName () {
+    try {
+        $branch = git rev-parse --abbrev-ref HEAD
+
+        if ($branch -eq "HEAD") {
+            # we're probably in detached HEAD state, so print the SHA
+            $branch = git rev-parse --short HEAD
+            Write-Host " ($branch)" -ForegroundColor "red"
+        } else {
+            # we're on an actual branch, so print it
+            Write-Host " ($branch)" -ForegroundColor "blue"
+        }
+    } catch {
+        # we'll end up here if we're in a newly initiated git repo
+        Write-Host " (no branches yet)" -ForegroundColor "yellow"
+    }
+}
+
+function prompt {
+    $base = "PS "
+    $path = "$($executionContext.SessionState.Path.CurrentLocation)"
+    $userPrompt = "$('>' * ($nestedPromptLevel + 1)) "
+
+    Write-Host "`n$base" -NoNewline
+
+    if (Test-Path .git) {
+        Write-Host $path -NoNewline -ForegroundColor "green"
+        Write-BranchName
+    } else {
+        # we're not in a repo so don't bother displaying branch name/sha
+        Write-Host $path -ForegroundColor "green"
+    }
+
+    return $userPrompt
+}
+
+# PowerShell Settings
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+
+# Env
+$env:BAT_THEME = "gruvbox-dark"
+
+
+# Aliases
+Set-Alias -Name k8s -Value kubectl
+
+function Display-Content () { bat -p --paging=never $args }
+Set-Alias -Name cat -Value Display-Content
+
+function List-Tree () { lsd --tree $args }
+function List-Long () { lsd -l $args }
+Set-Alias -Name tree -Value List-Tree
+Set-Alias -Name ll -Value List-Long
+Set-Alias -Name ls -Value lsd
